8b1850a783e9959f990beb70ef24bae9f8acb354
Hemersom Junior <hemersomjuniorhemersom@gmail.com>
2026-01-02 03:00:05 +0000

    As regras no bucket diretamente não deveria ser algo mais generico?

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -129,7 +129,7 @@ export default function SettingsPage() {
     if (!avatarFile || !user) return;
 
     setIsUploading(true);
-    const storageRef = ref(storage, `avatars/${user.id}/avatar.jpg`);
+    const storageRef = ref(storage, `avatars/${user.id}/${avatarFile.name}`);
 
     try {
       await uploadBytes(storageRef, avatarFile);
